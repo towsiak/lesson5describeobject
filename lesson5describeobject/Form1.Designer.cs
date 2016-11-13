274821134bac9933c3398870ce247a9592f44749
Tom Owsiak <towsiak@yahoo.com>
2016-11-12 22:52:54 -0500

    lesson 5 project files

---
+++ lesson5describeobject/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace lesson5describeobject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.descLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lesson5describeobject.Properties.Resources.vader;
+            this.pictureBox1.Location = new System.Drawing.Point(601, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 623);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // descLabel
+            // 
+            this.descLabel.AutoSize = true;
+            this.descLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descLabel.Location = new System.Drawing.Point(39, 45);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(0, 55);
+            this.descLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 712);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label descLabel;
+    }
+}
+
---
+++ lesson5describeobject/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson5describeobject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            decimal price = 24.99M;//use decimal data type for monetary values
+            bool hasSaber = true;//Use bool to represent true or false values
+            string color = "black";//Use string data types to represent values like words
+            double height = 6.5;//double data types are good for representing measured things
+            //line below produces a description shown in the form when the program runs
+            //\n has the result of stack the output vertically over several horizontal lines
+            descLabel.Text = $"Price:{price}\nHas Saber:{hasSaber}\nColor:{color}\nHeight:{height}";
+        }
+    }
+}
